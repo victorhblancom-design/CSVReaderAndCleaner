348db91ea545dbd6323d5af73387dc141852f774
victorhblancom-design <victorhblancom@gmail.com>
2026-05-29 20:54:07 -0600

    Add client cleanup filtering

--- DuplicateClientsCleanup/Account.cs
+++ DuplicateClientsCleanup/Account.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information........
 public class Account
 {
-    public ClientModel ClientObj { get; set; } 
-    public string AccountNumber { get; set; }
+    public ClientModel ClientObj { get; set; } = new();
+    public string AccountNumber { get; set; } = string.Empty;
     public bool ShouldDelete { get; set; }
 }
